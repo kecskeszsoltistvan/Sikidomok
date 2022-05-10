dbe89d1a397856e4faf7fd8e47716f3e43d56117
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2022-05-10 08:41:40 +0200

    Layout frissítés (Eredmények)

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ ablak1.config(background="#eeeeee")
 
 ms = Frame(ablak1)
 ms.grid(row=0, column=0)
-Label(
+v1 = Label(
     ablak1,
     text='Változó1:', 
     bg='#eeeeee'
 ).place(x=20, y=50)
-Label(
+v2 = Label(
     ablak1,
     text='Változó2:', 
     bg='#eeeeee'
 ).place(x=20, y=100)
-Label(
+v3 = Label(
     ablak1,
     text='Változó3:', 
     bg='#eeeeee'
@@ -41,6 +41,48 @@ b = ttk.Entry(ablak1)
 b.place(x=80, y=100)
 c = ttk.Entry(ablak1)
 c.place(x=80, y=150)
+e1 = Label(
+    ablak1,
+    text='Eredmény1:', 
+    bg='#eeeeee'
+).place(x=10, y=250)
+e2 = Label(
+    ablak1,
+    text='Eredmény2:', 
+    bg='#eeeeee'
+).place(x=10, y=280)
+e3 = Label(
+    ablak1,
+    text='Eredmény3:', 
+    bg='#eeeeee'
+).place(x=10, y=310)
+e4 = Label(
+    ablak1,
+    text='Eredmény4:', 
+    bg='#eeeeee'
+).place(x=220, y=250)
+e5 = Label(
+    ablak1,
+    text='Eredmény5:', 
+    bg='#eeeeee'
+).place(x=220, y=280)
+e6 = Label(
+    ablak1,
+    text='Eredmény6:', 
+    bg='#eeeeee'
+).place(x=220, y=310)
+ea = ttk.Entry(ablak1)
+ea.place(x=80, y=250)
+eb = ttk.Entry(ablak1)
+eb.place(x=80, y=280)
+ec = ttk.Entry(ablak1)
+ec.place(x=80, y=310)
+ed = ttk.Entry(ablak1)
+ed.place(x=290, y=250)
+ee = ttk.Entry(ablak1)
+ee.place(x=290, y=280)
+ef = ttk.Entry(ablak1)
+ef.place(x=290, y=310)
 canvas = Canvas(ablak1, height = 200, width = 300, bg = "white", bd = 10).place(x=250, y=20) # Canvas
 gomb = Button(ablak1, text = "Számítás", command = ablak1.destroy, height = 2, width = 28).place(x=20, y=200) # Gomb
 m1 = Menubutton(ms, text = "Fájl", underline = 0)
