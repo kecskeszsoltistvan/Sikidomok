31fd89d3687ce02978cda45e4ffbe27ec612af7e
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2022-05-18 13:24:46 +0200

    Egy szörnyű kódműtét kezdete

--- main.py
+++ main.py
@@ -1,6 +1,102 @@
 from email import message
 from tkinter import *
 from tkinter import ttk
+from math import *
+
+szamitas = "None"
+
+#Számolások
+ures=None
+s=''
+#negyzet
+def negyzet():
+    if not s:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    a=float(v1.get())
+    b=float(v2.get())
+    
+    if a==ures or b==ures:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    
+    if a<=0 or b<=0:
+            keredmeny.delete(0,END)
+            keredmeny.insert(0, str("pozitív szám kell"))
+    else:
+        #kerület
+        kerulet=2*(a+b)
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str(kerulet)+"cm2")
+        terulet=a*b
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str(terulet)+"cm3")
+
+#teglalap
+def teglalap():
+    if not s:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    a=float(v1.get())
+    b=float(v2.get())
+    
+    if a==ures or b==ures:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    
+    if a<=0 or b<=0:
+            keredmeny.delete(0,END)
+            keredmeny.insert(0, str("pozitív szám kell"))
+    else:
+        #kerület
+        kerulet=2*(a+b)
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str(kerulet)+"cm2")
+        terulet=a*b
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str(terulet)+"cm3")
+
+#rombusz
+def rombusz():
+    if not s:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    a=float(v1.get())
+    m=float(v2.get())
+    
+    if a==ures or m==ures:
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str("Szám adat kell"))
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str("Szám adat kell"))
+    
+    if a<=0 or m<=0:
+            keredmeny.delete(0,END)
+            keredmeny.insert(0, str("pozitív szám kell"))
+    else:
+        #kerület
+        kerulet=4*a
+        keredmeny.delete(0, END)
+        keredmeny.insert(0, str(kerulet)+"cm2")
+        terulet=a*b
+        teredmeny.delete(0, END)
+        teredmeny.insert(0, str(terulet)+"cm3")
+#Számolások vége. Menügombok.
+
+def negyzet_v():
+    jelenleg.set("Alakzat: Négyzet")
+
+#Menügombok vége. Ablak.
 
 def nevjegy():
     abl2 = Toplevel(ablak1)
@@ -25,31 +121,31 @@ v1 = Label(
     ablak1,
     text='Változó1:', 
     bg='#eeeeee'
-).place(x=20, y=50)
+).place(x=20, y=30)
 v2 = Label(
     ablak1,
     text='Változó2:', 
     bg='#eeeeee'
-).place(x=20, y=80)
+).place(x=20, y=60)
 v3 = Label(
     ablak1,
     text='Változó3:', 
     bg='#eeeeee'
-).place(x=20, y=110)
+).place(x=20, y=90)
 a = ttk.Entry(ablak1)
-a.place(x=80, y=50)
+a.place(x=80, y=30)
 b = ttk.Entry(ablak1)
-b.place(x=80, y=80)
+b.place(x=80, y=60)
 c = ttk.Entry(ablak1)
-c.place(x=80, y=110)
-e1 = Label(
+c.place(x=80, y=90)
+kerulet = Label(
     ablak1,
-    text='Eredmény1:', 
+    text='Kerület:', 
     bg='#eeeeee'
 ).place(x=10, y=250)
-e2 = Label(
+terulet = Label(
     ablak1,
-    text='Eredmény2:', 
+    text='Terület:', 
     bg='#eeeeee'
 ).place(x=10, y=280)
 e3 = Label(
@@ -57,41 +153,29 @@ e3 = Label(
     text='Eredmény3:', 
     bg='#eeeeee'
 ).place(x=10, y=310)
-e4 = Label(
-    ablak1,
-    text='Eredmény4:', 
-    bg='#eeeeee'
-).place(x=220, y=250)
-e5 = Label(
-    ablak1,
-    text='Eredmény5:', 
-    bg='#eeeeee'
-).place(x=220, y=280)
-e6 = Label(
-    ablak1,
-    text='Eredmény6:', 
-    bg='#eeeeee'
-).place(x=220, y=310)
-ea = ttk.Entry(ablak1)
-ea.place(x=80, y=250)
-eb = ttk.Entry(ablak1)
-eb.place(x=80, y=280)
+
+keredmeny = ttk.Entry(ablak1)
+keredmeny.place(x=80, y=250)
+teredmeny = ttk.Entry(ablak1)
+teredmeny.place(x=80, y=280)
 ec = ttk.Entry(ablak1)
 ec.place(x=80, y=310)
-ed = ttk.Entry(ablak1)
-ed.place(x=290, y=250)
-ee = ttk.Entry(ablak1)
-ee.place(x=290, y=280)
-ef = ttk.Entry(ablak1)
-ef.place(x=290, y=310)
 sym = PhotoImage(file="ikon_nagy.png")
 kep = ttk.Label(
     ablak1,
     image=sym
 ).place(x=440, y=250)
-ef.place(x=290, y=310)
 canvas = Canvas(ablak1, height = 200, width = 300, bg = "white", bd = 10).place(x=250, y=20) # Canvas
-szamitas = Button(ablak1, text = "Számítás", command = ablak1.destroy, height = 1, width = 16, font = "Impact 20").place(x=15, y=150) # Gomb
+szamitas = Button(ablak1, text = "Számítás", command = ablak1.destroy, height = 1, width = 11, font = "Impact 28").place(x=17, y=125) # Gomb
+# Milyen alakzatot csinálsz éppen?
+jelenleg = StringVar()
+jelenleg.set = (ablak1, "Alakzat: -", "jelenlegi állapot")
+jelenlegl = Label(
+    ablak1,
+    text=jelenleg, 
+    bg='#eeeeee'
+).place(x=15, y=220)
+
 Manual = Button(ablak1, text = "Olvassel megnyitása", command = ablak1.destroy, height = 2, width = 30).place(x=195, y=350) # Gomb
 Button(ablak1, text = "Kilépés", command = ablak1.destroy, height = 2, width = 20).place(x=20, y=350) # Gomb
 
@@ -104,7 +188,7 @@ m1.config(menu = fm)
 m2 = Menubutton(ms, text="Síkidomok", underline = 0)
 m2.pack(side = LEFT)
 tm = Menu(m2)
-tm.add_command(label="Négyzet",command=ablak1.destroy, underline = 0)
+tm.add_command(label="Négyzet",command=negyzet_v, underline = 0)
 tm.add_command(label="Téglalap",command=ablak1.destroy, underline = 0)
 m2.config(menu = tm)
 ablak1.mainloop()
